9aa59747dd63a7fa19a842aa25f61f8420539c47
Ben Wuebker <yallsobad@protonmail.com>
2023-04-13 23:41:59 -0500

    updated styling for mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,28 @@
 import { SiYourtraveldottv } from "react-icons/si";
+import { useState, useEffect } from "react";
 
 function Navbar() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      setWindowWidth(window.innerWidth);
+    });
+  }, []);
+
   return (
     <>
       <nav className="nav-container">
         <SiYourtraveldottv className="logo" />
 
         <ul className="menu">
-          <li className="menu-item">Destinations</li>
-          <li className="menu-item">About</li>
-          <li className="menu-item">Contact</li>
+          {windowWidth > "430" && (
+            <>
+              <li className="menu-item">Destinations</li>
+              <li className="menu-item">About</li>
+              <li className="menu-item">Contact</li>
+            </>
+          )}
           <button className="sign-up">Sign Up</button>
         </ul>
       </nav>
